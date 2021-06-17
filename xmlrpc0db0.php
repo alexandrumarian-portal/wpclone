c61cfe9bb4e5fdbe75c02f9097f50d2f1876be21
CIOTLOS Alexandru Marian OSE <Alexandru.Ciotlos@office.orange.intra>
2021-06-17 23:32:22 +0300

    wordpress clone just for html,css,js

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://dulceata.ro</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://dulceata.ro/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://dulceata.ro/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://dulceata.ro/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://dulceata.ro/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://dulceata.ro/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
